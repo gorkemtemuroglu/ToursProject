82d06b207ac46439979676bec9a772761670ae73
gorkem temuroglu <gorkem.temuroglu@icloud.com>
2023-11-02 11:48:00 +0300

    Sending real complex email handler with pugs

--- utils/email.js
+++ utils/email.js
@@ -1,25 +1,58 @@
 const nodemailer = require('nodemailer');
+const pug = require('pug');
+const htmlToText = require('html-to-text');
 
-const sendMail = async (options) => {
-  const transporter = nodemailer.createTransport({
-    //  service: 'Gmail',
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-  });
+module.exports = class Email {
+  constructor(user, url) {
+    this.to = user.email;
+    this.firstName = user.name.split(' ')[0];
+    this.url = url;
+    this.from = `Görkem Temuroğlu <${process.env.EMAIL_FROM}>`;
+  }
 
-  const mailOptions = {
-    from: 'Görkem Temuroğlu <hello@gorkem.io>',
-    to: options.email,
-    subject: options.subject,
-    text: options.message,
-    //  html:
-  };
+  newTransport() {
+    if (process.env.NODE_ENV === 'production') {
+      //Sendgrid
+      return 1;
+    }
 
-  await transporter.sendMail(mailOptions);
+    return nodemailer.createTransport({
+      //  service: 'Gmail',
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
+  }
+
+  // send the actual mail
+  async send(template, subject) {
+    // 1) Render HTML based on pug templates.
+    const html = pug.renderFile(
+      `${__dirname}/../views/emails/${template}.pug`,
+      {
+        firstName: this.firstName,
+        url: this.url,
+        subject,
+      },
+    );
+
+    // 2) Define email options
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      subject,
+      html,
+      text: htmlToText.htmlToText(html),
+    };
+
+    // 3) Create a transport and send email
+    await this.newTransport().sendMail(mailOptions);
+  }
+
+  async sendWelcome() {
+    await this.send('welcome', 'Welcome to the natours family.');
+  }
 };
-
-module.exports = sendMail;
