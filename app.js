9a42b2311e4ab641e311bffae6166dc3c7edd023
gorkem temuroglu <gorkem.temuroglu@icloud.com>
2023-11-03 10:34:31 +0300

    Implemented CORS

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -26,6 +27,18 @@ app.enable('trust proxy');
 
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
+
+// Implement cors
+app.use(cors());
+// Access-Control-Allow-Origin *
+// backend api.natours.com, front-end natours.com
+// app.use(
+//   cors({
+//     origin: 'https://www.natours.com',
+//   }),
+// );
+
+app.options('*', cors());
 
 // Serving static files...
 app.use(express.static(path.join(__dirname, 'public')));
